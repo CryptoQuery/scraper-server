608453e5b441a50ce2f40ce7b71b30de652818b3
Philip Smith <smithp1992@gmail.com>
2017-11-12 14:42:38 -0800

    - Fixed endpoint functionality. - Added getArticles - Updated old getArticles to getArticleByUrl

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,22 @@ var router = express.Router();
 
 var lib = require('../lib/index.js');
 
+var middleware = function(req, res, next) {
+  lib.auth.decrypt(req.body).then(function (result) {
+    req.auth = result;
+    next();
+  }).catch(function (error) {
+    res.status(400).json({
+      success: false,
+      response: 'auth failed'
+    });
+  })
+};
+
 
 //DONE: addArticle <link> <published_at> <author> <title> <article>
-router.post('/addArticle', function (req, res, next) {
-  lib.auth.decrypt(req.body).then(function (result) {
-    return lib.articles.addArticle(result);
-  }).then(function (result) {
+router.post('/addArticle', middleware, function (req, res, next) {
+  lib.articles.addArticle(req.auth).then(function (result) {
     res.json({
       success: true,
       response: result
@@ -21,7 +31,7 @@ router.post('/addArticle', function (req, res, next) {
   });
 });
 
-//DONE: getArticle <[url]>
+//DONE: getArticles <[article_id]>
 router.post('/getArticles', function (req, res, next) {
   lib.articles.getArticles(req.body).then(function (result) {
     res.json({
@@ -36,7 +46,22 @@ router.post('/getArticles', function (req, res, next) {
   });
 });
 
-//CHECK: getArticlesByDate <start_date> <end_date>
+//DONE: getArticlesByUrl <[url]>
+router.post('/getArticlesByUrl', function (req, res, next) {
+  lib.articles.getArticlesByUrl(req.body).then(function (result) {
+    res.json({
+      success: true,
+      response: result
+    });
+  }).catch(function (error) {
+    res.status(400).json({
+      success: false,
+      response: 'request failed'
+    });
+  });
+});
+
+//DONE: getArticlesByDate <start_date> <end_date>
 router.post('/getArticlesByDate', function (req, res, next) {
   lib.articles.getArticlesByDate(req.body).then(function (result) {
     res.json({
